6ee66e602272864d064a4a0dd33507fd55acdf08
Agnes Heppich <agnhe421@student.liu.se>
2018-05-03 10:24:35 +0200

    updated

--- src/textReader/textReader.java
+++ src/textReader/textReader.java
@@ -1,37 +1,47 @@
 package textReader;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 public class textReader {
 
-		public static void main(String[] args) throws IOException {
+		public static void main(String[] args) throws IOException{
 			
 		ArrayList<String> list = new ArrayList<String> ();
-		List<byte[]> hashes = new ArrayList<byte[]>(list.size());
+		Map dictionary = new HashMap(); 
+		int hash = 0; 
 		
-			//Read file from folder text 
-			File f1 = new File(".\\.\\.\\text\\01.txt");
-			File f2 = new File (".\\.\\.\\text\\02.txt"); 
-			File f3 = new File (".\\.\\.\\text\\03.txt"); 
+		File f1 = new File(".\\.\\.\\text\\01.txt");
+		File f2 = new File (".\\.\\.\\text\\02.txt"); 
+		File f3 = new File (".\\.\\.\\text\\03.txt"); 
 			
-			BufferedReader in = (new BufferedReader(new FileReader(f1)));
-			String line;
-			String processedLine="";
+		//Create bufferedReader to read text from file
+		BufferedReader in = (new BufferedReader(new FileReader(f1)));
+		String line;
+		String processedLine="";
 			
 			//Remove punctuation and convert to lower case
 			while ((line = in.readLine()) != null) {
 			    processedLine = line.replaceAll("[^a-zA-Z0-9]"," ").toLowerCase().replaceAll("( )+", " ");
-			    System.out.print(processedLine);
-			
-				for (String word : line.split("\\s+")) {
+			   // System.out.print(processedLine);
+			    
+			    //Add words to list 
+				for (String word : processedLine.split("\\s+")) {
 					  list.add(word);
-					}
-			
-				
-			
-			}
 
+						  if(! dictionary.containsKey(word)) {
+							  dictionary.put(word, hash);
+							  hash++; 
+						  }
+						  	
+					  
+					}
+				
+				
+			}
+			
+	System.out.print(list); 
+	System.out.println(""); 
+	System.out.print(dictionary);
+				 
 		}
 		
 }
