f2fd8dfb6439e694c8cb02b5b823bb8b17cc4b34
Agnes Heppich <agnhe421@student.liu.se>
2018-05-03 09:28:44 +0200

    update

--- src/textReader/textReader.java
+++ src/textReader/textReader.java
@@ -1,25 +1,37 @@
 package textReader;
-	import java.io.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class textReader {
 
-
 		public static void main(String[] args) throws IOException {
+			
+		ArrayList<String> list = new ArrayList<String> ();
+		List<byte[]> hashes = new ArrayList<byte[]>(list.size());
 		
-		
-			File f1 = new File("H:\\tnm098\\tnm098\\lab3\\Lab3.2\\lab3\\text\\01.txt");
+			//Read file from folder text 
+			File f1 = new File(".\\.\\.\\text\\01.txt");
+			File f2 = new File (".\\.\\.\\text\\02.txt"); 
+			File f3 = new File (".\\.\\.\\text\\03.txt"); 
 			
 			BufferedReader in = (new BufferedReader(new FileReader(f1)));
 			String line;
 			String processedLine="";
+			
+			//Remove punctuation and convert to lower case
 			while ((line = in.readLine()) != null) {
 			    processedLine = line.replaceAll("[^a-zA-Z0-9]"," ").toLowerCase().replaceAll("( )+", " ");
 			    System.out.print(processedLine);
-		
+			
+				for (String word : line.split("\\s+")) {
+					  list.add(word);
+					}
+			
+				
+			
 			}
-	
-			
-			
-			
+
 		}
 		
 }
